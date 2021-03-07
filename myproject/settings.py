94717314695691e70548d9aa3dc77575d20b54b5
jabir15 <jabir.hussain.aec@gmail.com>
2021-03-07 04:39:10 +0100

    forgot about localhost

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foodrider.pythonanywhere.com']
 
 
 # Application definition
@@ -126,4 +126,10 @@ LOGOUT_REDIRECT_URL = 'index'
 
 RAZORPAY_API = os.getenv("RAZORPAY_API")
 RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'assamcollegecode.pythonanywhere@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
